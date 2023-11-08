9e63ac464dac7ff788da5cd1aacb56b2e27f099c
josht8601 <josht8601@gmail.com>
2023-11-08 08:13:33 -0500

    front-end view

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,23 +1,64 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import SensorChart from './SensorChart';
 import './App.css';
 
 function App() {
+  const [sensorData, setSensorData] = useState({
+    temperature: '--',
+    soilMoisture: '--',
+    humidity: '--',
+  });
+  const [temperatureData, setTemperatureData] = useState([]);
+  const [humidityData, setHumidityData] = useState([]);
+  const [moistureData, setMoistureData] = useState([]);
+
+  useEffect(() => {
+    // Create WebSocket connection.
+    const socket = new WebSocket('ws://localhost:8080');
+
+    // Connection opened
+    socket.addEventListener('open', function (event) {
+      console.log('Connected to the WebSocket server');
+    });
+
+    // Listen for messages
+    socket.addEventListener('message', function (event) {
+      const data = JSON.parse(event.data);
+      setSensorData(data);
+    });
+
+    // Clean up on unmount
+    return () => {
+      socket.close();
+    };
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Sensor Data Dashboard</h1>
+        <div className="grid-container">
+          <div className="grid-item">
+            <span className="data-label">Temperature:</span>
+            <span className="data-value">{sensorData.temperature}°C</span>
+          </div>
+          <div className="grid-item">
+            <span className="data-label">Soil Moisture:</span>
+            <span className="data-value">{sensorData.soilMoisture}%</span>
+          </div>
+          <div className="grid-item">
+            <span className="data-label">Humidity:</span>
+            <span className="data-value">{sensorData.humidity}%</span>
+          </div>
+        </div>
       </header>
+      <main>
+        <div className="chart-container">
+          <SensorChart title="Temperature" data={temperatureData} />
+          <SensorChart title="Humidity" data={humidityData} />
+          <SensorChart title="Soil Moisture" data={moistureData} />
+        </div>
+      </main>
     </div>
   );
 }
---
+++ my-app/src/SensorChart.js
@@ -0,0 +1,83 @@
+import React from 'react';
+import { Line } from 'react-chartjs-2';
+import 'chart.js/auto';
+
+const SensorChart = ({ title, data }) => {
+  const chartData = {
+    labels: data.map(d => d.time),
+    datasets: [
+      {
+        label: title,
+        data: data.map(d => d.value),
+        fill: true,
+        backgroundColor: 'rgba(75, 192, 192, 0.2)',
+        borderColor: 'rgb(75, 192, 192)',
+        borderWidth: 2,
+        tension: 0.3, // This will make the line a bit smoother
+        pointRadius: 0, // Remove the points for a cleaner look
+      },
+    ],
+  };
+
+  const chartOptions = {
+    responsive: true,
+    maintainAspectRatio: false,
+    plugins: {
+      title: {
+        display: true,
+        text: title,
+        position: 'top',
+        align: 'start',
+        font: {
+          size: 20,
+          weight: 'bold',
+        },
+        padding: {
+          top: 10,
+          bottom: 30,
+        },
+      },
+      legend: {
+        display: false,
+      },
+    },
+    scales: {
+      x: {
+        title: {
+          display: true,
+          text: 'Time (seconds)',
+        },
+      },
+      y: {
+        min: 0,
+        max: 100,
+        title: {
+          display: true,
+          text: `Value (${getUnits(title)})`,
+        },
+      },
+    },
+  };
+
+  // Helper function to get units based on the title
+  function getUnits(title) {
+    switch(title.toLowerCase()) {
+      case 'temperature':
+        return '°C';
+      case 'humidity':
+        return '%';
+      case 'soil moisture':
+        return '%';
+      default:
+        return '';
+    }
+  }
+
+  return (
+    <div style={{ height: '400px', width: '100%' }}> {/* Increase the height of the chart container */}
+      <Line data={chartData} options={chartOptions} />
+    </div>
+  );
+};
+
+export default SensorChart;
